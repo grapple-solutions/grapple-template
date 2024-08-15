2f731cb69b341a4df800ffc96892e318225adf0b
Patrick Riegler <patrick.riegler@grapple-solutions.com>
2024-08-15 12:56:18 +0200

    first draft of sintio-zev

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,21 @@
 
   // demofullclassicmodelsasas
   let el1;
-  import("App/Employees/count").then((module) => {
+  import("App/Organisations").then((module) => {
     const Count = module.default;
     new Count({
       target: el1,
       props: {
+        translation: {
+          Actions: "Aktionen",
+          name: "Name des Unternehmens",
+          id: "Kennung",
+          "No more items":
+            "Es gibt keine weiteren Elemente mit diesen Kriterien",
+          "Load More": "Weitere Elemente anzeigen",
+          "New": "Neues Unternehmen",
+          "Select All Items": "alle Unternehmen selektieren"
+        },
         // filter: { where:{ "postalCode":44000 } },
         // css: "background-color: rgb(204 90 113 / var(--tw-bg-opacity));"
         // css: "display: none;"
@@ -16,32 +26,28 @@
   });
 
   let el2;
-  import("App/Employees/findById").then((module) => {
+  import("App/Users/findById").then((module) => {
     // import("App/Orders/updateById").then((module) => {
     const FindById = module.default;
     new FindById({
       target: el2,
       props: {
-        id: 1002,
+        id: "7f72cb00-5ad8-11ef-bca9-b1e91a7f9854",
         schema: {
           "field-properties": {
             "field-order": [
-              "id",
-              "customerName",
-              "contactLastName",
-              "contactFirstName",
-              "postalCode",
-              "city",
-              "country",
+              "name",
+              "address",
+              "organisations",
+              "moveindate"
             ],
-            "hidden-fields": ["state", "addressLine2", "creditLimit"],
+            "hidden-fields": ["id"],
             "date-fields": [
-              { name: "orderDate", type: "date" },
-              { name: "shippedDate", type: "time" },
+              { name: "moveindate", type: "date" },
             ],
           },
         },
-        // dateFormat: 'DD.MM.YYYY - HH:mm',
+        dateFormat: 'DD.MM.YYYY',
         // css: "color: orange;  font-weight: bold; border-color: orange; ",
         // css: "display: none;"
       },
@@ -49,7 +55,7 @@
   });
 
   let el3;
-  import("App/Employees/find").then((module) => {
+  import("App/Users").then((module) => {
     const Find = module.default;
     new Find({
       target: el3,
@@ -59,52 +65,58 @@
         schema: {
           "field-properties": {
             "field-order": [
+              "name",
+              "address",
+              "organisations",
+              "moveindate",
               "id",
-              "customerName",
-              "contactLastName",
-              "contactFirstName",
-              "postalCode",
-              "city",
-              "country",
             ],
-            "hidden-fields": ["state", "addressLine2", "creditLimit"],
+            // "hidden-fields": ["id"],
             "date-fields": [
-              { name: "orderDate", type: "date" },
-              { name: "shippedDate", type: "time" },
+              { name: "moveindate", type: "date" },
             ],
           },
         },
         translation: {
-          customerName: "Name des Kunden",
-          contactLastName: "Nachname",
-          contactFirstName: "Vorname",
-          postalCode: "PLZ",
-          city: "Ort",
+          Actions: "Aktionen",
+          name: "Name des Benutzers",
+          address: "Adresse",
+          organisations: "Unternehmen",
+          moveindate: "Beginn der Nutzung",
+          id: "Kennung",
           "No more items":
             "Es gibt keine weiteren Elemente mit diesen Kriterien",
           "Load More": "Weitere Elemente anzeigen",
+          "New": "Neuen Benutzer",
+          "Select All Items": "alle Benutzer selektieren"
         },
-        // dateFormat: 'DD.MM.YYYY - HH:mm',
+        dateFormat: 'DD.MM.YYYY',
         // // useLocalTimezone: true,
-        enableFilter: false,
-        enableLoadMore: false,
-        // css: "color: orange;  font-weight: bold; border-color: orange; ",
-        // css: "font-size: 1vw; min-width: 8rem; padding:0.1rem; padding-left:.5rem; color: orange;  font-weight: bold; border-color: lightgray;",
+        enableFilter: true,
         // css: "display: none;"
       },
     });
   });
 </script>
 
-<div class="max-w-6xl m-auto felx p-8">
+<div class="max-w-6xl m-auto felx p-8" style="padding:20px">
   <!-- <div class="text-3xl">Customers, This is from the svelte application</div> -->
 
   <div class="p-8 grid gap-3">
     <div>
-      <!-- <h2 class="text-xl">Count</h2> -->
-      <div class="uimodule" bind:this={el1} style="width:350px;" />
-      <div class="uimodule" bind:this={el2} />
-      <div class="uimodule" bind:this={el3} />
+      <h2 class="text-xl">Unternehmen (organisations):</h2>
+      <div class="uimodule1" bind:this={el1} />
+
+      <br /><br /><hr style="border-top-width:10px;" /><br /><br />
+
+      <h2 class="text-xl">Benutzer (users):</h2>
+      <div class="uimodule3" bind:this={el3} />
+
+      <br /><br /><hr style="border-top-width:10px;" /><br /><br />
+
+      <h2 class="text-xl">einzelner Benutzer (user):</h2>
+      <div class="uimodule2" bind:this={el2} />
+
     </div>
   </div>
 </div>
